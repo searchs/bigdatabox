28ea3e2ddda3a717022b8e33f1a447bfa572a183
Ola Ajibode <searchs@users.noreply.github.com>
2018-06-24 21:48:57 +0100

    Run on local and cluster commands added

--- wordcount.py
+++ wordcount.py
@@ -2,6 +2,14 @@ from nltk.tokenize import wordpunct_tokenize
 from pyspark import SparkConf, SparkContext
 import sys
 
+"""Running commands:
+Spark 2(local instance): spark-submit word_count.py local wiki.txt  wc-count
+Spark 2(local instance): spark-submit word_count.py "local[4]" wiki.txt  wc-count
+
+Spark 2(cluster instance): spark-submit word_count.py spark://dlrc-headnode:7077 \
+                        hdfs://dlrc-headnode/wiki-txt hdfs://dlrc-headnode/users/devola/wc-output
+"""
+
 if len(sys.argv) < 4:
     print(">>sys.stderr", "Usage: %s master input output" % sys.argv[0])
     exit(1)
