99cb74beb4d8e6e72b0dd125c42c92d0145afe29
Ola Ajibode <ola.ajibode@gmail.com>
2018-12-09 23:56:29 +0000

    Streaming sample

--- spark_scla_ramps.scala
+++ spark_scla_ramps.scala
@@ -29,3 +29,22 @@ hiveDF.write.saveAsTable("New_Table_Name")
 
 
 val students = List(Students(1, "Jacobs", 21, "M"), Students(2, "Jane", 23, "F"),Students(3, "David", 19, "M"),Students(4, "Ade", 20, "M"),Students(5, "Nike", 22, "F"),Students(6, "Britney", 22,"F"))
+
+
+
+//STREAMING SECTION
+//Create a local StreamingContext with 2 thread and batch interval 2 sec  
+val sparkConf = new SparkConf().setAppName("NetworkCount")
+val ssc = new StreamingContext(conf, Second(1))
+
+//Create a Dstream that will connect to host:port
+val lines = ssc.socketTextStream(args(0), args(1).toInt, StorageLevel.MEMORY_AND_DISK_SER)
+
+//Start our StreamingContext and wait for it to finish
+ssc.start()
+//Wait for the job to finish
+ssc.awaitTermination()
+
+//Run in the commandline/terminal - using SparkScala example
+//Bash: run-example streaming.NetworkCount localhost 1234
+//Bash: nc -L -p 1234
