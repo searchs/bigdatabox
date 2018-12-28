77db2df024daec4ad73eb5c7d3188374c82dc927
Ola Ajibode <ola.ajibode@gmail.com>
2018-12-28 16:46:30 +0000

    Create db_table_cleaner.sh

---
+++ db_table_cleaner.sh
@@ -0,0 +1,35 @@
+
+#!/bin/bash
+#ADHOC Queries:  We don't need these tables once reports are verified OK by analysts
+# Default:  Cleans upto 30 days worth of process tables
+# lookback:  Go back this number of days to cleanup report tables.
+# Usage: sh ./run_db_cleanup.sh -l 30
+
+echo "Adhoc Table Cleanup starts...."
+echo $(date --utc)
+
+while getopts l: option
+do
+ case "${option}"
+ in
+ l) lookback=${OPTARG};;
+ esac
+done
+
+process_date=$(date --utc +'%Y%m%d')
+
+echo "Process Date: $process_date"
+echo "Lookback Days: $lookback"
+
+#while loop using the lookback as the limit
+COUNTER=0
+         while [  $COUNTER -le $lookback ]; do
+             echo The counter is $COUNTER
+             cleanup_date=$(date --utc -d "$process_date - $COUNTER days" +'%Y%m%d')
+             echo "Cleanup Date: $cleanup_date"
+             hive -hiveconf cleanup_date=$cleanup_date -f /app/$USER/scripts/adhocs/db_cleanup.hql
+             let COUNTER=COUNTER+1
+         done
+echo "================================================================"
+echo "End of Cleanup of Reports Redundant tables. $(date --utc)"
+echo "================================================================"
