30cd6b93584824f92adfda700468f2ddc526cc8f
Ola Ajibode <878566+searchs@users.noreply.github.com>
2019-11-25 11:54:24 +0000

    adding SparkSession example

--- pyspark_starter.py
+++ pyspark_starter.py
@@ -1,3 +1,4 @@
+#Using SparkContext
 from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setMaster("local[2]").setAppName("RDD Example")
@@ -24,3 +25,27 @@ joined_rdd = left.join(right)
 collected = joined_rdd.collect()
 
 collected #[('Alfred', (4, 5)), ('Richard', (1, 2))]
+
+
+# Using SparkSession
+# Notice we’re using pyspark.sql library here
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+        .master("local") \
+        .appName("CSV file loader") \
+        .getOrCreate()
+
+# couple ways of setting configurations
+#spark.conf.set("spark.executor.memory", '8g')
+#spark.conf.set('spark.executor.cores', '3')
+#spark.conf.set('spark.cores.max', '3')
+#spark.conf.set("spark.driver.memory", '8g')
+
+file_path = "./AB_NYC_2019.csv"
+# Always load csv files with header=True
+df = spark.read.csv(file_path, header=True)
+
+df.printSchema()
+
+df.select('neighbourhood').distinct().show(10, False)
