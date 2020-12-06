4bd4e60a1dabffc929bf9dffd77f26140176eccd
Ola Ajibode <ola.ajibode@gmail.com>
2020-12-06 16:12:07 +0000

    Create SparkContextInitializer

---
+++ SparkContextInitializer.scala
@@ -0,0 +1,24 @@
+package com.katchstyle.nlytics
+
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+object SparkContextInitializer  {
+
+def createSparkContext(name: String) = {
+  val sc: SparkConf = basicSparkConf(name)
+  val spark = new SparkContext(sc)
+  spark.setLogLevel("WARN")
+  spark
+}
+
+
+def basicSparkConf(name: String): SparkConf = {
+  new SparkConf()
+    .setAppName(name)
+    .set("spark.io.compression.codec", "lzf")
+    .set("spark.speculation", "true")
+}
+
+}
